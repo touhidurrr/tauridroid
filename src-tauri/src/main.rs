805ca46f19707b4d2eea33204bce9b8d3fdfc34b
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2023-01-13 21:14:07 +0600

    Add files via upload

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,28 @@
+use tauridroid::AppBuilder;
+
+#[cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+pub fn main() {
+    AppBuilder::new().run();
+}
+
+// #![cfg_attr(
+//     all(not(debug_assertions), target_os = "windows"),
+//     windows_subsystem = "windows"
+// )]
+
+// // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//     format!("Hello, {}! You've been greeted from Rust!", name)
+// }
+
+// fn main() {
+//     tauri::Builder::default()
+//         .invoke_handler(tauri::generate_handler![greet])
+//         .run(tauri::generate_context!())
+//         .expect("error while running tauri application");
+// }
